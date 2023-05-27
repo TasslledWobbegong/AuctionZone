8c9cd9c1e00c724f73d0a34d1ecd4a821c14fe1e
Vander <vander@Vanders-MacBook-Pro.local>
2023-05-27 14:07:55 -0700

    filled out more components

--- client/components/AuctionDisplay.jsx
+++ client/components/AuctionDisplay.jsx
@@ -3,13 +3,36 @@
 import React from 'react';
 import AuctionItem from './AuctionItem.jsx';
 
-const AuctionDisplay = props => {
-return (
-    <div className='displayBox'>
-        <h4>Auction Items</h4>
-        <AuctionItem/>
-    </div>
+
+
+
+const itemMaker = (item, index, props) => (
+    <AuctionItem
+        {...item}
+        increaseBid={() => props.increaseBid(item.itemID)}
+        buyout={() => props.buyout(item.itemID)}
+        delete={() => props.delete(item.itemID)}
+        key={index}
+        itemTitle={item.itemTitle}
+        initialPrice={item.itemInitialPrice}
+        currPrice={item.itemCurrentPrice}
+        itemDes={item.itemDescription}
+        itemDeadline={item.itemDeadline}
+        itemBuyout={item.itemBuyoutPrice}
+        image={item.itemImage}
+    />
 )
+
+
+const AuctionDisplay = props => {
+    return (
+        <div className='displayBox'>
+            <h4>Auction Items</h4>
+            <div className='allAuctionItems'>
+                {props.itemList.map((item, index) => itemMaker(item, index, props))}
+            </div>
+        </div>
+    )
 
 }
 
---
+++ client/components/AuctionItem.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const AuctionItem = props => (
+    <div className='auctionitems'>
+
+        <div>
+            <label className='title'>{props.itemTitle}</label>
+        </div>
+        <div>
+            <img src={this.image}></img>
+        </div>
+        <div>
+            <span className='price'>Price: {props.currPrice}</span>
+            <span className='deadline'>Deadline: {props.itemDeadline}</span>
+        </div>
+        <div>
+            <p className='buynow'>Buy It Now: {props.itemBuyout}</p>
+        </div>
+        <div>
+            <p>description: {props.itemDes}</p>
+        </div>
+        <div>
+            <button onClick={() => props.increaseBid(props.itemID)}>Add Bid</button>
+            <button onClick={() => props.increaseBid(props.itemID)}>Buyout</button>
+            <button onClick={() => props.delete(props.itemID)}>Delete Item</button>
+        </div>
+    </div>
+
+);
+export default AuctionItem;
+
+/*
+itemTitle={item.itemTitle}
+        initialPrice={item.itemInitialPrice}
+        currPrice={item.itemCurrentPrice}
+        itemDes={item.itemDescription}
+        itemDeadline={item.itemDeadline}
+        itemBuyout={item.itemBuyoutPrice}
+        image={item.itemImage}
+*/
