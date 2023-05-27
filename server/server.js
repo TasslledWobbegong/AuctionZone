908e1064c3a7df31f26669a8fe2f512a0b6dec14
Ian Weinholtz <109614480+itsHackinTime@users.noreply.github.com>
2023-05-27 11:06:02 -0500

    add schema for item and boiler plate for server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
 // AUCTION ZONE!
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
