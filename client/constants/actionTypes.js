d8e05ee134ba1bf78a554cafe7fc52ed99be007d
jojecameron <jojecameron@gmail.com>
2023-05-27 10:20:37 -0700

    created action constants and actionCreators

--- client/constants/actionTypes.js
+++ client/constants/actionTypes.js
@@ -1,1 +1,6 @@
 // add action type constants
+
+export const MAKE_BID = 'MAKE_BID';
+export const BUYOUT_ITEM = 'BUYOUT_ITEM';
+export const DELETE_ITEM = 'DELETE_ITEM';
+export const POST_ITEM = 'POST_ITEM';
