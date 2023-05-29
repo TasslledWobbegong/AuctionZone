c76720ac48d4c1e6fdeb72dbc04335c46dca6b75
Vander <vander@Vanders-MacBook-Pro.local>
2023-05-29 10:44:51 -0700

    started adding  browser router

--- client/App.jsx
+++ client/App.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import './styles/App.scss';
-import AuctionContainer from './containers/AuctionContainer';
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import MainContainer from './containers/MainContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 class App extends Component {
-    render() {
-      return (
-        <div className='App'>
-            <div className='pages'>
-                <h1>AUCTION ZONE is here to stay!</h1>
-                <AuctionContainer/>
-            </div>
+  render() {
+    return (
+      <div className='App'>
+        <div className='pages'>
+          <h1>AUCTION ZONE is here to stay!</h1>
+          <Routes>
+            <Route path="/" element={<MainContainer />} />
+          </Routes>
         </div>
-      );
-    }
+      </div>
+    );
   }
-  
-  export default App;
+}
+
+export default App;
